1c78f77af7946323fdaf1c2ef06ec13c37e8060f
Chiamaka Nwafor <nwaforchiamaka548@gmail.com>
2022-10-13 05:56:43 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+*main - Printing in C
+*Return: Always 0 (Successful in C)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
