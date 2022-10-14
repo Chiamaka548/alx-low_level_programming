006d974c5d147738a77c4b0cd45fedb2448435e9
Chiamaka Nwafor <nwaforchiamaka548@gmail.com>
2022-10-14 06:25:30 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,13 +7,11 @@
 
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
+	char c;
 
-	for (i = 0; i < 26; i * *)
-	{
-		putchar(alp[i]);
-	}
+	for (c = 'a'; c < = 'z'; c++)
+		putchar(c);
+
 	putchar('\n');
 	return (0);
 }
