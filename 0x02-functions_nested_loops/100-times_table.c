b3c2a897ff20fbac561ce8925116273ef12feff8
Chiamaka Nwafor <nwaforchiamaka548@gmail.com>
2022-10-18 17:03:18 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,48 +1,49 @@
 #include "main.h"
 
 /**
- * print_times_table - Prints a multipication table for n
+ * print_times_table - Prints a multiplication table up to param
  * @n: The number to be treated
  *
- * Return: Nothing.
+ * Return: Number matrix
  */
-
 void print_times_table(int n)
 {
 	int x, y, z;
 
-	if (!(n > 15 || n < 0))
+	if (n >= 0 && n <= 14)
 	{
 		for (x = 0; x <= n; x++)
 		{
 			for (y = 0; y <= n; y++)
 			{
-				z = (x * y);
-				if (y != 0)
+				z = x * y;
+				if (z > 99)
 				{
 					_putchar(',');
-					_putchar(' ');
-				}
-				if (z < 10 && y != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((z % 10) + '0');
-				}
-				else if (z >= 10 && z < 100)
-				{
-					_putchar(' ');
-					_putchar((z / 10) + '0');
-					_putchar((z % 10) + '0');
-				}
-				else if (z >= 100 && y != 0)
-				{
+					_putchar(32);
 					_putchar((z / 100) + '0');
-					_putchar((z / 10) % 10 + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else if (z > 9)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
+					_putchar(((z / 10) % 10) + '0');
 					_putchar((z % 10) + '0');
 				}
 				else
-					_putchar((z % 10) + '0');
+				{
+					if (y != 0)
+					{
+						_putchar(',');
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(z + '0');
+				}
 			}
 			_putchar('\n');
 		}
