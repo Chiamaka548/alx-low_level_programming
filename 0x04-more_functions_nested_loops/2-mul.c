afde5f104532bd5b47513f8c6b455bec6939096e
Chiamaka Nwafor <nwaforchiamaka548@gmail.com>
2022-10-24 07:55:36 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: The first number to be multiplied
+ * @x: The second number to be multiplied with the first number
+ *
+ * Return: The result of multiplication between a param and x param
+ */
+
+int mul(int a, int x)
+{
+	return (a * x);
+}
