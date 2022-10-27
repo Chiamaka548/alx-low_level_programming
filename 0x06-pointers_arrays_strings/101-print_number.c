cdaf1e0b200e7ac3dc37be2bc2475310fc2da1e4
Chiamaka Nwafor <nwaforchiamaka548@gmail.com>
2022-10-27 08:56:54 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,21 +2,36 @@
 
 /**
  * print_number - Prints an integer
- * @n: integer to be printed
+ * @a: number to be printed
+ *
+ * Return: void
  */
 
-void print_number(int n)
+void print_number(int a)
 {
-	unsigned int num = n;
+	unsigned int m, d, count;
 
-	if (n < 0)
+	if (a < 0)
 	{
-		_putchar('-');
-		num = -num;
+		_putchar(45);
+		m = a * -1;
+	}
+	else
+	{
+		m = a;
 	}
 
-	if ((num / 10) > 0)
-		print_number(num / 10);
+	d = m;
+	count = 1;
 
-	_putchar((num % 10) + '0');
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
 }
