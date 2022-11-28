a47fce125e15cc72103b0ffde89228f15c6d3ada
Chiamaka Nwafor <nwaforchiamaka548@gmail.com>
2022-11-28 12:11:49 -0800

    2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -17,7 +17,8 @@ listint_t *add_nodeint(listint_t **head, const int n)
 		return (NULL);
 	ptr = malloc(sizeof(listint_t));
 	if (ptr == NULL)
-		ptr->n = n;
+		return (NULL);
+	ptr->n = n;
 	ptr->next = *head;
 	*head = ptr;
 	return (ptr);
